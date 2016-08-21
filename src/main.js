74cf9c7bf84ed445cfa50dae658e547a621a3648
Greg Babiars <gbabiars@gmail.com>
2016-08-20 22:55:03 -0700

    Handle events and refactor
    Added logic to handle the click events on buttons.
    
    Refactored to use model-view-intent pattern.

--- src/main.js
+++ src/main.js
@@ -4,34 +4,47 @@ import { makeDOMDriver, div, h1 } from '@cycle/dom';
 
 require('file?name=[name].[ext]!./index.html');
 
-function main() {
-  return {
-    DOM: xs.of(
-      div('.calculator', [
-        div('.result', '0'),
-        div('.row', [
-          div('.button', '1'),
-          div('.button', '2'),
-          div('.button', '3'),
-        ]),
-        div('.row', [
-          div('.button', '4'),
-          div('.button', '5'),
-          div('.button', '6')
-        ]),
-        div('.row', [
-          div('.button', '7'),
-          div('.button', '8'),
-          div('.button', '9')
-        ]),
-        div('.row', [
-          div('.button', ' '),
-          div('.button', '0'),
-          div('.button', ' '),
+function intent(domSource) {
+  return domSource.select('.button').events('click')
+    .filter(event => event.target.textContent !== '')
+    .map(event => parseInt(event.target.textContent, 10));
+}
 
-        ])
+function model(buttonSelect$) {
+  return buttonSelect$.startWith(0);
+}
+
+function view(state$) {
+  return state$.map(value =>
+    div('.calculator', [
+      div('.result', `${value}`),
+      div('.row', [
+        div('.button', '1'),
+        div('.button', '2'),
+        div('.button', '3'),
+      ]),
+      div('.row', [
+        div('.button', '4'),
+        div('.button', '5'),
+        div('.button', '6')
+      ]),
+      div('.row', [
+        div('.button', '7'),
+        div('.button', '8'),
+        div('.button', '9')
+      ]),
+      div('.row', [
+        div('.button'),
+        div('.button', '0'),
+        div('.button'),
       ])
-    )
+    ])
+  );
+}
+
+function main(sources) {
+  return {
+    DOM: view(model(intent(sources.DOM)))
   };
 }
 
