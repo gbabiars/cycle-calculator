53887577dd17f933ab67b6a7be1f70158c28520c
Greg Babiars <gbabiars@gmail.com>
2016-08-21 07:26:28 -0700

    Add operator row

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import { makeDOMDriver, div, h1 } from '@cycle/dom';
 require('file?name=[name].[ext]!./index.html');
 
 function intent(domSource) {
-  return domSource.select('.button').events('click')
-    .filter(event => event.target.textContent !== '')
+  return domSource.select('.value').events('click')
     .map(event => parseInt(event.target.textContent, 10));
 }
 
@@ -19,24 +18,28 @@ function view(state$) {
     div('.calculator', [
       div('.result', `${value}`),
       div('.row', [
-        div('.button', '1'),
-        div('.button', '2'),
-        div('.button', '3'),
+        div('.button.value', '1'),
+        div('.button.value', '2'),
+        div('.button.value', '3'),
+        div('.button.operator', '/')
       ]),
       div('.row', [
-        div('.button', '4'),
-        div('.button', '5'),
-        div('.button', '6')
+        div('.button.value', '4'),
+        div('.button.value', '5'),
+        div('.button.value', '6'),
+        div('.button.operator', '*')
       ]),
       div('.row', [
-        div('.button', '7'),
-        div('.button', '8'),
-        div('.button', '9')
+        div('.button.value', '7'),
+        div('.button.value', '8'),
+        div('.button.value', '9'),
+        div('.button.operator', '-')
       ]),
       div('.row', [
-        div('.button'),
-        div('.button', '0'),
-        div('.button'),
+        div('.button.placeholder'),
+        div('.button.value', '0'),
+        div('.button.placeholder'),
+        div('.button.operator', '+')
       ])
     ])
   );
