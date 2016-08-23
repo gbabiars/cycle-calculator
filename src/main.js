35ac07b5b9328f5461832909225409ed8097543c
Greg Babiars <gbabiars@gmail.com>
2016-08-22 21:18:27 -0700

    Add computation logic

--- src/main.js
+++ src/main.js
@@ -4,42 +4,113 @@ import { makeDOMDriver, div, h1 } from '@cycle/dom';
 
 require('file?name=[name].[ext]!./index.html');
 
-function intent(domSource) {
-  return domSource.select('.value').events('click')
-    .map(event => parseInt(event.target.textContent, 10));
+const CLEAR = 'C';
+const ADD = '+';
+const SUBTRACT = '-';
+const MULTIPLY = '*';
+const DIVIDE = '/';
+const EQUALS = '=';
+
+const defaults = {
+  total: 0,
+  buffer: '',
+  operator: ADD
+};
+
+function calculateTotal(operator, previousTotal, bufferValue) {
+  if(operator === ADD) {
+    return previousTotal + bufferValue;
+  }
+  if(operator === SUBTRACT) {
+    return previousTotal - bufferValue;
+  }
+  if(operator === MULTIPLY) {
+    return previousTotal * bufferValue;
+  }
+  if(operator === DIVIDE) {
+    return previousTotal / bufferValue;
+  }
+  return previousTotal;
 }
 
-function model(buttonSelect$) {
-  return buttonSelect$.startWith(0);
+function intent(domSource) {
+  const numberClick$ = domSource.select('.value').events('click')
+    .map(event => parseInt(event.target.textContent, 10));
+  const operatorClick$ = domSource.select('.operator').events('click')
+    .map(event => event.target.textContent);
+  return { numberClick$, operatorClick$ };
+}
+
+function model({ numberClick$, operatorClick$ }) {
+  const number$ = numberClick$.startWith(0);
+  const operator$ = operatorClick$.startWith(ADD);
+  const buffer$ = xs.merge(numberClick$, operatorClick$)
+    .fold((prev, input) => {
+      if(Number.isInteger(input)) {
+        return {
+          total: prev.total,
+          buffer: `${prev.buffer}${input}`,
+          operator: prev.operator
+        };
+      }
+
+      if(input === CLEAR) {
+        return defaults;
+      }
+
+      if(prev.buffer === '') {
+        return {
+          total: prev.total,
+          buffer: '',
+          operator: input
+        };
+      }
+
+      if(prev.operator === EQUALS) {
+        return {
+          total: parseInt(prev.buffer, 10),
+          buffer: '',
+          operator: input
+        };
+      }
+
+      return {
+          total: calculateTotal(prev.operator, prev.total, parseInt(prev.buffer, 10)),
+          buffer: '',
+          operator: input
+      };
+    }, defaults);
+
+  return buffer$.debug();
 }
 
 function view(state$) {
-  return state$.map(value =>
+  return state$.map(({ total, buffer, operator }) =>
     div('.calculator', [
-      div('.result', `${value}`),
+      div('.result', `${buffer === '' ? total : buffer}`),
       div('.row', [
         div('.button.value', '1'),
         div('.button.value', '2'),
         div('.button.value', '3'),
-        div('.button.operator', '/')
+        div('.button.operator', DIVIDE)
       ]),
       div('.row', [
         div('.button.value', '4'),
         div('.button.value', '5'),
         div('.button.value', '6'),
-        div('.button.operator', '*')
+        div('.button.operator', MULTIPLY)
       ]),
       div('.row', [
         div('.button.value', '7'),
         div('.button.value', '8'),
         div('.button.value', '9'),
-        div('.button.operator', '-')
+        div('.button.operator', SUBTRACT)
       ]),
       div('.row', [
-        div('.button.placeholder'),
+        div('.button.operator', CLEAR),
         div('.button.value', '0'),
-        div('.button.placeholder'),
-        div('.button.operator', '+')
+        div('.button.operator', EQUALS),
+        div('.button.operator', ADD)
       ])
     ])
   );
